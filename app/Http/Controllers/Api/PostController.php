7d06b2879d2f44bbc7c90e07a8aeacfb55750553
nancy <smithnancy@outlook.com>
2023-07-08 21:34:46 +1000

    Post resouce created to transform some fields ad created_at and content

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
     public function index(){
-        return Post::all();
+        // this resource is a wraper in top of the eloquent  respose,it will transform each field in anything you want,
+        // Eg the data in any format we want, date formated, paragraphs
+        return PostResource::collection(Post::all());
     }
 }
