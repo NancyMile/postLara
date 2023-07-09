955c02c251db529591af78f5b803211962595f69
nancy <smithnancy@outlook.com>
2023-07-09 14:40:44 +1000

    post controller   query  with category relationship, to return the category

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,8 +10,11 @@ use App\Http\Resources\PostResource;
 class PostController extends Controller
 {
     public function index(){
+
+        $posts = Post::with('category')->paginate(10);
+
         // this resource is a wraper in top of the eloquent  respose,it will transform each field in anything you want,
         // Eg the data in any format we want, date formated, paragraphs
-        return PostResource::collection(Post::paginate(10));
+        return PostResource::collection($posts);
     }
 }
