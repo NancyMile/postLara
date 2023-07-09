82acb8554907bd68e722d7d3c90e8b95e11554a1
nancy <smithnancy@outlook.com>
2023-07-09 15:45:03 +1000

    categories composable created

---
+++ resources/js/composables/categories.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { ref } from "vue";
+
+export default function useCategories() {
+
+    const categories = ref({})
+
+    const getCategories = async () => {
+        axios.get('/api/categories')
+            .then(response => {
+                categories.value = response.data.data;
+    }).catch (error => console.log(error));
+}
+    
+
+
+
+    return {
+        categories,
+        getCategories
+    }
+
+}
