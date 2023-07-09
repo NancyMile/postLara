e9a55e55dbfcb2f6ec8fb4f18434cbcec0bb097b
nancy <smithnancy@outlook.com>
2023-07-09 13:59:36 +1000

    post and  category relationship added on mmodels

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable =['title', 'content'];
+    protected $fillable =['title', 'content','category_id'];
+
+    public function categoy(){
+        return $this->belongsTo(Category::class);
+    }
 }
