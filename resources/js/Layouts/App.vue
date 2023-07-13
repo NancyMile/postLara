d8985a014c5be5345598ad5afade90015c90f2f2
nancy <smithnancy@outlook.com>
2023-07-13 13:07:34 +1000

    layout app has been created, contain the main navigation

---
+++ resources/js/Layouts/App.vue
@@ -0,0 +1,19 @@
+<script>
+import { Link } from '@inertiajs/vue3';
+
+export default {
+
+    components: {
+        Link
+    }
+}
+</script>
+<template>
+    <div class="sm:flex gap-4 sm:top-0 sm:left-0  p-6 text-right z-10">
+        <Link href="posts" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</Link>
+        <Link href="about" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">About</Link>
+    </div>
+    <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
+    <slot/>
+    </div>
+</template>
