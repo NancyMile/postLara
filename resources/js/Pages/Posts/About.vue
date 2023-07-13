ec87e1463a98820ecd8f4b10d9dc3303d872a9e1
nancy <smithnancy@outlook.com>
2023-07-13 13:08:31 +1000

    duplicated navigation code  removed  now  importing the app loyout

--- resources/js/Pages/Posts/About.vue
+++ resources/js/Pages/Posts/About.vue
@@ -1,21 +1,16 @@
 <script>
-import { Link } from '@inertiajs/vue3';
-
+import AppLayout from '../../Layouts/App.vue'
 export default {
     components: {
-        Link
+        AppLayout
     }
 }
 </script>
 
 <template>
- <div class="sm:flex gap-4 sm:top-0 sm:left-0  p-6 text-right z-10">
-    <Link :href="route('posts')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</Link>
-    <Link :href="route('about')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">About</Link>
-</div>
-    <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
+    <AppLayout>
         <div class="mb-4 w-1/4">
             About Us!
         </div>
-    </div>
+    </AppLayout>
 </template>
--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,15 +1,13 @@
 <script>
-import { Link } from '@inertiajs/vue3';
+import AppLayout from "../../Layouts/App.vue"
 import usePosts from '../../composables/posts'
 import useCategories from '../../composables/categories'
 import { ref, onMounted, watch } from 'vue';
 
 export default {
-
     components: {
-        Link
+        AppLayout
     },
-
     props: {
         posts: Object
     }
@@ -53,12 +51,7 @@ export default {
 </script>
     
 <template>
-    <div class="sm:flex gap-4 sm:top-0 sm:left-0  p-6 text-right z-10">
-            <Link href="posts" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</Link>
-            <Link href="about" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">About</Link>
-        </div>
-    <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
-        
+    <AppLayout>
         <div class="mb-4 w-1/4">
             <select
                 v-model="selectedCategory"
@@ -172,5 +165,5 @@ export default {
                 @pagination-change-page="getPosts"
             />
         </div>
-    </div>
+    </AppLayout>
 </template>
