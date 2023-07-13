12424ae937a952913a3c5158e58ebe9d3d6f32df
nancy <smithnancy@outlook.com>
2023-07-13 12:00:29 +1000

    Post controller created and adder to web route now using inertia

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    public function index(){
+
+        $orderColumn = request('order_column','created_at');
+        if(!in_array($orderColumn,['id','title','created_at'])){
+            $orderColumn = 'created_at';
+        }
+        $orderDirection = request('order_direction', 'desc');
+        if(!in_array($orderDirection,['asc','desc'])){
+            $orderDirection = 'desc';
+        }
+
+        $posts = Post::with('category')
+            ->when(request('category'), function($query){
+                $query->where('category_id',request('category'));
+            } )
+            ->orderBy($orderColumn, $orderDirection)
+            ->paginate(10);
+
+        // this resource is a wraper in top of the eloquent  respose,it will transform each field in anything you want,
+        // Eg the data in any format we want, date formated, paragraphs
+        //return PostResource::collection($posts);
+
+
+        $posts = PostResource::collection($posts);
+
+        return inertia('Posts/Index', compact('posts'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,5 +35,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('posts',[PostController::class,'index'])->name('posts');
+Route::inertia('about','Posts/About')->name('about');
+
 
 require __DIR__.'/auth.php';
