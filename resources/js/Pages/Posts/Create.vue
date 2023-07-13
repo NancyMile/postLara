de71f6c7559c37b6250e9d7326657fe1d66e43c5
nancy <smithnancy@outlook.com>
2023-07-13 14:28:02 +1000

    page  post  for new post created

---
+++ resources/js/Pages/Posts/Create.vue
@@ -0,0 +1,22 @@
+<script>
+import AppLayout from '../../Layouts/App.vue'
+import { Head } from '@inertiajs/vue3';
+
+export default {
+    components: {
+        AppLayout,
+        Head
+    }
+}
+</script>
+
+<template>
+    <Head>
+        <Title>Create</Title>
+    </Head>
+    <AppLayout>
+        <div class="mb-4 w-1/4">
+            New post!
+        </div>
+    </AppLayout>
+</template>
